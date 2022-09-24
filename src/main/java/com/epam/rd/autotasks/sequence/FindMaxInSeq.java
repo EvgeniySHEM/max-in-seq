9c4b2c19d386742df69cb9aa55455d942481539b
Evgeniy Sharychenkov <sanctio@yandex.ru>
2022-09-24 13:19:28 +0300

    first commit

--- src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
+++ src/main/java/com/epam/rd/autotasks/sequence/FindMaxInSeq.java
@@ -3,10 +3,20 @@ import java.util.Scanner;
 
 public class FindMaxInSeq {
     public static int max() {
-
-        // Put your code here
-
-        return 0;
+        int max = Integer.MIN_VALUE;
+        String num = (new Scanner(System.in).nextLine());
+        String[] numArray = num.split(" ");
+        int[] numbers = new int[numArray.length];
+        for (int i = 0; i < numArray.length; i++) {
+            numbers[i] = Integer.parseInt(numArray[i]);
+            if (numbers[i] == 0) {
+                break;
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
